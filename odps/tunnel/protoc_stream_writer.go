664fae89a2e775ce67a15dee5afffc69e5ff7df0
Bill Cipher <xulongfetion@gmail.com>
2025-03-13 21:31:36 +0800

    use pooled buff

--- odps/tunnel/protoc_stream_writer.go
+++ odps/tunnel/protoc_stream_writer.go
@@ -23,6 +23,13 @@ import (
 	"google.golang.org/protobuf/encoding/protowire"
 )
 
+var bufPool = &sync.Pool{
+	New: func() interface{} {
+		return make([]byte, 0, 10)
+	},
+}
+
+
 type ProtocStreamWriter struct {
 	inner io.Writer
 }
@@ -38,20 +45,26 @@ func (r *ProtocStreamWriter) WriteTag(num protowire.Number, typ protowire.Type) 
 }
 
 func (r *ProtocStreamWriter) WriteVarint(v uint64) error {
-	b := protowire.AppendVarint(nil, v)
+	b := bufPool.Get().([]byte)
+	b = protowire.AppendVarint(b, v)
 	err := writeFull(r.inner, b)
+	bufPool.Put(b[:0])
 	return err
 }
 
 func (r *ProtocStreamWriter) WriteFixed32(val uint32) error {
-	b := protowire.AppendFixed32(nil, val)
+	b := bufPool.Get().([]byte)
+	b = protowire.AppendFixed32(b, val)
 	err := writeFull(r.inner, b)
+	bufPool.Put(b[:0])
 	return err
 }
 
 func (r *ProtocStreamWriter) WriteFixed64(val uint64) error {
-	b := protowire.AppendFixed64(nil, val)
+	b := bufPool.Get().([]byte)
+	b = protowire.AppendFixed64(b, val)
 	err := writeFull(r.inner, b)
+	bufPool.Put(b[:0])
 	return err
 }
 
